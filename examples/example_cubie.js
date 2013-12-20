5b754fd8def1a33cfb8cf0b3b56af168e97d4cc1
Alexander Lorz <bigalex@gmx.de>
2013-12-20 17:08:45 +0100

    example for cubie board

---
+++ examples/example_cubie.js
@@ -0,0 +1,24 @@
+var Gpio = require('../onoff').Gpio,
+    //led = new Gpio(17, 'out'), // 38
+    button = new Gpio(1, 'in', 'both'); // 117
+
+button.watch(function(err, value) {
+    if (err) exit();
+    console.log("Button pressed");
+    //led.writeSync(value);
+});
+
+function exit() {
+    //led.unexport();
+    button.unexport();
+    process.exit();
+}
+
+process.on('SIGINT', exit);
+
+button.read(function(err, value) {  // Asynchronous read.
+        if (err) throw err;
+        console.log("value: " + value);
+});
+
+
